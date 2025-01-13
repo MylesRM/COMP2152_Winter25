4f254bbeb60d768bbcb409f0210dbbc9fd758f0b
MylesRM <mylesromans@gmail.com>
2025-01-13 14:47:49 -0500

    lab1

---
+++ Week01/Lab/Coding Questions/lab1.py
@@ -0,0 +1,20 @@
+# Sample Coding Questions 01 Week 01
+# Name: Myles Romans Muirhead
+
+# Defining Variables
+var_array = [1, 4, 7, 9]
+
+#Order of Operations
+a, b, c, d = 1, 2, 3, 4
+
+e= (a - (b** c) // d + (a % c))
+
+# Formatting
+
+temperature = 32.6
+print(f"The temperature today is: {temperature:.3f} degrees Celsius")
+
+# Common Functions
+userAge= int(input("Enter your age: "))
+print("Now showing the shop items filtered by age: {0}".format(userAge))
+
